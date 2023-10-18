cd902d91529f2b65abf1b9725ea63e3ab1a68592
akmerey <96563104+akmerey@users.noreply.github.com>
2023-10-18 10:32:27 +0600

    Add files via upload

---
+++ scripts/deploy.js
@@ -0,0 +1,21 @@
+
+const hre = require("hardhat");
+
+async function main() {
+  const Chai = await hre.ethers.getContractFactory("chai"); //fetching bytecode and ABI
+  const chai = await Chai.deploy(); 
+  
+  await chai.deployed();//deploying your smart contract
+
+  console.log("Deployed contract address:",`${chai.address}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
+
+
